a25f1af27b2a48623302524276419818505e9322
Manna <khmannaict13@gmail.com>
2019-07-03 21:43:40 +0600

    commit all file

--- StepperView/ViewController.swift
+++ StepperView/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var seg: UISegmentedControl!
+    
+    @IBAction func segAction(_ sender: UISegmentedControl) {
+        if(seg.selectedSegmentIndex == 0){
+            label.isHidden = false
+            img.isHidden = false
+            
+            label.text = "Manna"
+            img.image = #imageLiteral(resourceName: "i4")
+            
+            
+        }
+        if(seg.selectedSegmentIndex == 1){
+            label.isHidden = false
+            img.isHidden = false
+            label.text = "Munna"
+            img.image = #imageLiteral(resourceName: "i5")
+        }
+        if(seg.selectedSegmentIndex == 2){
+            label.isHidden = false
+            img.isHidden = false
+            label.text = "Shaon"
+            img.image = #imageLiteral(resourceName: "i1")
+        }
+        if(seg.selectedSegmentIndex == 3){
+            label.isHidden = true
+            img.isHidden = true
+            label.text = "Lalon"
+            img.image = #imageLiteral(resourceName: "i3")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.isHidden = true
+        img.isHidden = true
+     
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    
 }
 
